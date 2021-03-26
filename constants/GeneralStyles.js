2b992de5d6faa88b37331fe24c76318d63691fdc
jarroot <jarroot1@gmail.com>
2021-03-26 13:08:17 +0300

    added GeneralStyles

---
+++ constants/GeneralStyles.js
@@ -0,0 +1,34 @@
+export default {
+	flex_centered: {
+		alignItems: "center", 
+		justifyContent: "center" 
+	},
+
+	row_wrap: {
+		flexDirection: "row",
+		flexWrap: "wrap"
+	},
+
+	row_ac: {
+		flexDirection: "row", 
+		alignItems: "center", 
+	},
+
+	row_ac_jfe: {
+		flexDirection: "row",
+		alignItems: "center",
+		justifyContent: "flex-end"
+	},
+
+	row_centered: {
+		flexDirection: "row", 
+		alignItems: "center", 
+		justifyContent: "center" 
+	},
+
+	row_center_around: {
+		flexDirection: "row", 
+		alignItems: "center", 
+		justifyContent: "space-around" 
+	}
+}
