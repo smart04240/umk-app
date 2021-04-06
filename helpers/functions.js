61d601067435a24cacaaf868b493e7ca19ec21c0
jarroot <jarroot1@gmail.com>
2021-04-06 12:27:53 +0300

    minor fix

--- helpers/functions.js
+++ helpers/functions.js
@@ -2,6 +2,6 @@ export const ucfirst = str => str.substr(0, 1).toUpperCase() + str.substr(1);
 
 export const isFunction = func => toString.call( func ) === "[object Function]";
 
-export const isObject = obj = toString.call( obj ) === "[object Object]";
+export const isObject = obj => toString.call( obj ) === "[object Object]";
 
 export const getTranslated = (field, locale) => isObject( field ) ? ( field[ locale ] || "" ) : field;
