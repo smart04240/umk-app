0906396f8e0eacb9dabdaa673dd55cb3b97cb385
Dmytro Kardava <dima.kardava@gmail.com>
2021-04-16 17:02:20 +0300

    update Main to support additional props

--- components/general/Main.js
+++ components/general/Main.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { View } from 'react-native';
 import useThemeStyles from '../../hooks/useThemeStyles';
 
-export default props => {
+export default function Main({style, children, ...rest}) {
 
     const ThemeStyles = useThemeStyles();
 
 	return (
-		<View style={[ { backgroundColor: ThemeStyles.main_bg, flex: 1 }, props.style || {} ]} >
-			{ props.children }
+		<View style={[ { backgroundColor: ThemeStyles.main_bg, flex: 1 }, style || {} ]} {...rest} >
+			{ children }
 		</View>
 	)
 }
