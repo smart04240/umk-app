4043ad5b5d396a1e9dd0a311c29b9e115b43568c
Dmytro Kardava <dima.kardava@gmail.com>
2021-04-23 15:39:02 +0300

    fix padding screen breakpoint

--- constants/Layout.js
+++ constants/Layout.js
@@ -1,7 +1,7 @@
 import { Dimensions  } from "react-native";
 
 const window = Dimensions.get('window');
-const paddingHorizontal = window.width < 420 ? 15 : 23 
+const paddingHorizontal = window.width < 375 ? 15 : 23
 
 export default {
 	...window,
