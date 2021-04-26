f5ef910bc457871128cda80707187e460ff1e9be
GeorgeHope <georgehopekek@gmail.com>
2021-04-26 12:58:53 +0200

    Added withHeaderConfig component

--- components/profile/ProfileMain.js
+++ components/profile/ProfileMain.js
@@ -4,36 +4,34 @@ import { View } from "react-native";
 import Translations from '../../constants/Translations';
 import Layout from "../../constants/Layout";
 import Routes from "../../constants/Routes";
-
 import useTranslated from '../../hooks/useTranslated';
 import Button from '../form/Button';
-import TopBox from '../general/TopBox';
 import ProfileAvatar from './ProfileAvatar';
 import ProfileMainInfo from './ProfileMainInfo';
-import Tabs from '../general/Tabs';
+import WithHeaderConfig from "../layout/WithHeaderConfig";
 
 const ProfileMain = props => {
-
 	const navigation = useNavigation();
 	const avatar_size = Math.floor( Layout.width * 0.308 );
 
-
 	return (
-		<View style={{padding: 15}}>
+		<WithHeaderConfig borderless={true}>
+			<View style={{padding: 15}}>
 
-			<View style={{ flexDirection: "row", marginBottom: 18 }}>
+				<View style={{ flexDirection: "row", marginBottom: 18 }}>
 
-				<View style={{ marinRight: 10 }}>
-					<ProfileAvatar size={ avatar_size > 132 ? 132 : avatar_size }/>
+					<View style={{ marinRight: 10 }}>
+						<ProfileAvatar size={ avatar_size > 132 ? 132 : avatar_size }/>
+					</View>
+
+					<ProfileMainInfo />
 				</View>
 
-				<ProfileMainInfo />
+				<Button onPress={ () => navigation.navigate( Routes.ProfileEdit )} transparent_bg={ true }>
+					{ useTranslated( Translations.EditProfile )}
+				</Button>
 			</View>
-
-			<Button onPress={ () => navigation.navigate( Routes.ProfileEdit )} transparent_bg={ true }>
-				{ useTranslated( Translations.EditProfile )}
-			</Button>
-		</View>
+		</WithHeaderConfig>
 	)
 }
 
