9a2f257a23a283e464f40073637b6460686af0c1
jarroot <jarroot1@gmail.com>
2021-05-10 11:46:53 +0300

    RoundedImage minor fix

--- components/general/RoundedImage.js
+++ components/general/RoundedImage.js
@@ -3,18 +3,15 @@ import { ImageBackground, View } from 'react-native';
 
 const RoundedImage = props => {
 
-	const { img_source, size } = props;
-	const width = size || 77;
-	const height = size || 77;
-	const border_radius = size ? size / 2 : 77 / 2;
-
-	const sizes = { width, height, borderRadius: border_radius };
+	const { img_source } = props;
+	const size = props.size || 77;
+	const size_styles = { width: size, height: size, borderRadius: size / 2 };
 
 	if ( !img_source ) return null;
 
 	return (
-		<View style={[ { overflow: "hidden", marginRight: 11 }, sizes, props.box_style || {} ]}>
-			<ImageBackground style={[ { resizeMode: "cover" }, sizes ]} source={ img_source } />
+		<View style={[ { overflow: "hidden", marginRight: 11 }, size_styles, props.box_style || {} ]}>
+			<ImageBackground style={[ { resizeMode: "cover" }, size_styles ]} source={ img_source } />
 		</View>
 	)
 }
