b265dd1d5fd9ee7e1a36555350a11951459630d2
GeorgeHope <georgehopekek@gmail.com>
2021-05-13 10:17:58 +0200

    Refactor rankings to swiper component

--- screens/rankings/RankingsScreen.jsx
+++ screens/rankings/RankingsScreen.jsx
@@ -1,28 +1,26 @@
-import React from "react";
-import { View, Text } from "react-native";
-
+import React, {useMemo} from "react";
 import GeneralStyles from "../../constants/GeneralStyles";
 import Translations from "../../constants/Translations";
 import useThemeStyles from "../../hooks/useThemeStyles";
-
 import MainWithNavigation from "../../components/general/MainWithNavigation";
-import Tabs from "../../components/general/Tabs";
-import TopBox from "../../components/general/TopBox";
 import Dropdown from "../../components/form/Dropdown";
-import ContainerWithScroll from "../../components/general/ContainerWithScroll";
 import Layout from "../../constants/Layout";
 import RankingBox from "../../components/ranking/RankingBox";
 import useTranslator from "../../hooks/useTranslator";
+import Swiper from "react-native-screens-swiper";
+import shadowGenerator from "../../helpers/shadowGenerator";
+import Container from "../../components/general/Container";
+import WithHeaderConfig from "../../components/layout/WithHeaderConfig";
 
 const ranking_sample = {
 	number: 1,
 	avatar_source: require("../../assets/images/avatar.jpg"),
 	user_nickname: "nick_studenta1234",
 	badges: 12,
-	points: 254 
+	points: 254
 }
 
-export default function RankingsScreen() {
+const Faculty = () => {
 	const translate = useTranslator();
 	const ThemeStyles = useThemeStyles();
 
@@ -31,41 +29,84 @@ export default function RankingsScreen() {
 		rankings.push({ ...ranking_sample, number: i + 1 })
 	}
 
+	return(
+		<Container>
+			<Dropdown
+				label={ translate( Translations.FilterSelectDirection )}
+				options_box_style={{ maxHeight: Layout.height * 0.5 }}
+				options={[
+					{ value: "*", label: "Wszystkie" },
+					{ value: 1, label: "Option 1" },
+				]}
+			/>
+
+			{rankings && !!rankings.length && rankings.map(ranking => <RankingBox key={ranking.number} {...ranking} />)}
+		</Container>
+	)
+}
+
+export default function RankingsScreen() {
+	const translate = useTranslator();
+	const theme = useThemeStyles();
+
+	const rankings = [];
+	for ( let i = 0; i < 15; i++) {
+		rankings.push({ ...ranking_sample, number: i + 1 })
+	}
+
+	const screens = [
+		{
+			tabLabel: translate(Translations.Faculty),
+			component: Faculty
+		},
+		{
+			tabLabel: translate(Translations.Annual),
+			component: Faculty
+		},
+	];
+
+	const styles = useMemo(() => {
+		return (
+			{
+				pillContainer: {
+					backgroundColor: theme.box_bg,
+					borderBottomRightRadius: 15,
+					borderBottomLeftRadius: 15,
+					...shadowGenerator(5),
+				},
+				borderActive: {
+					borderColor: theme.blue_text
+				},
+				pillLabel: {
+					...GeneralStyles.text_regular,
+					textTransform: "uppercase",
+					color: theme.blue_text
+				},
+				activeLabel: {
+					...GeneralStyles.text_bold,
+					textTransform: "uppercase",
+					color: theme.blue_text
+				},
+				pillsOverflow: {
+					overflow: 'hidden',
+					height: 80
+				}
+			}
+		)
+	},[theme]);
+
 	return (
 		<MainWithNavigation>
-
-			<TopBox>
-				<Text style={[ 
-					GeneralStyles.text_regular,
-					{ color: ThemeStyles.dark_blue_text, marginBottom: 25 } 
-				]}>
-					{ translate( Translations.ChooseARanking )}
-				</Text>
-
-				<Tabs
-					style={{ justifyContent: "space-around" }}
-					tabs={[ translate( Translations.Faculty ), translate( Translations.Annual ) ]}
+			<WithHeaderConfig borderless={true}>
+				<Swiper
+					data={screens}
+					style={styles}
+					scrollableContainer={true}
+					stickyHeaderEnabled={true}
+					isStaticPills={true}
+					stickyHeaderIndex={0}
 				/>
-			</TopBox>
-
-			<ContainerWithScroll>
-
-				<Dropdown
-					label={ translate( Translations.FilterSelectDirection )}
-					options_box_style={{ maxHeight: Layout.height * 0.5 }}
-					options={[
-						{ value: "*", label: "Wszystkie" },
-						{ value: 1, label: "Option 1" },
-					]}
-				/>
-
-				<View style={{ marginTop: 20 }}>
-					{ rankings && !!rankings.length &&
-						rankings.map( ranking => <RankingBox key={ ranking.number } {...ranking } />)
-					}
-				</View>
-
-			</ContainerWithScroll>
+			</WithHeaderConfig>
 		</MainWithNavigation>
 	)
 }
