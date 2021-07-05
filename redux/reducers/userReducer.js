4f152ccf6fd906a06334c75d4a82bddcd7c30c32
GeorgeHope <georgehopekek@gmail.com>
2021-07-05 14:19:01 +0200

    WIP Connect user profile to api

--- redux/reducers/userReducer.js
+++ redux/reducers/userReducer.js
@@ -2,34 +2,37 @@ import {createReducer} from "@reduxjs/toolkit";
 import Actions from "../Actions";
 
 const user_init = {
-	nick_name: '',
-	avatar: '',
-	token: null,
-	role: ''
+    nick_name: '',
+    avatar: '',
+    token: null,
+    role: ''
 }
 
 export default createReducer(user_init, builder => {
-	builder
-		.addCase(Actions.User.Registered, ((state, action) => {
-			state.nick_name = action.payload;
-			state.isUnregistered = false;
-		}))
-		.addCase(Actions.User.Logout, () => null)
-		.addCase(Actions.User.USOSOAuth, (state, {payload}) => {
-			// console.log('USOSOAuth', {...state, ...payload});
+    builder
+        .addCase(Actions.User.Registered, ((state, action) => {
+            state.nick_name = action.payload;
+            state.isUnregistered = false;
+        }))
+        .addCase(Actions.User.Logout, () => null)
+        .addCase(Actions.User.USOSOAuth, (state, {payload}) => {
+            // console.log('USOSOAuth', {...state, ...payload});
 
-			return {...state, ...payload};
-		})
-		.addCase(Actions.User.USOSAccessToken.fulfilled, (state, {payload}) => {
-			// console.log('USOSAccessToken', {...state, ...payload});
+            return {...state, ...payload};
+        })
+        .addCase(Actions.User.USOSAccessToken.fulfilled, (state, {payload}) => {
+            // console.log('USOSAccessToken', {...state, ...payload});
 
-			return {...state, ...payload};
-		})
-		.addCase(Actions.API.DataLoaded, (state, {payload: {profile}}) => ({
-			...state,
-			avatar_url: profile.avatar_url,
-			user_name: `${profile.user_name}`,
-			studies: profile.studies,
-			graduation_dates: profile.graduation_dates
-		}))
+            return {...state, ...payload};
+        })
+        .addCase(Actions.API.DataLoaded, (state, {payload: {profile}}) => ({
+            ...state,
+            avatar_url: profile.avatar_url,
+            user_name: `${profile.user_name}`,
+            studies: profile.studies,
+            graduation_dates: profile.graduation_dates
+        }))
+        .addCase(Actions.User.Update, (state, {payload}) => {
+            state.user_name = payload
+        })
 });
--- screens/profile/EditProfileScreen.jsx
+++ screens/profile/EditProfileScreen.jsx
@@ -1,13 +1,11 @@
 import React from "react";
-import { Text, TouchableOpacity, View } from "react-native";
-import { useNavigation } from "@react-navigation/core";
-import { useSelector } from "react-redux";
-
+import {Alert, Text, TouchableOpacity, View} from "react-native";
+import {useNavigation} from "@react-navigation/core";
+import {useDispatch, useSelector} from "react-redux";
 import useThemeStyles from "../../hooks/useThemeStyles";
 import GeneralStyles from "../../constants/GeneralStyles";
 import Translations from "../../constants/Translations";
 import Routes from "../../constants/Routes";
-
 import TopBox from "../../components/general/TopBox";
 import ProfileAvatar from "../../components/profile/ProfileAvatar";
 import Input from "../../components/form/Input";
@@ -15,51 +13,76 @@ import Button from "../../components/form/Button";
 import MainWithNavigation from "../../components/general/MainWithNavigation";
 import ContainerWithScroll from "../../components/general/ContainerWithScroll";
 import useTranslator from "../../hooks/useTranslator";
+import API from "../../helpers/API";
+import Actions from "../../redux/Actions";
 
-export default function EditProfileScreen(props) {
-	const translate = useTranslator();
+export default function EditProfileScreen() {
+    const translate = useTranslator();
     const ThemeStyles = useThemeStyles();
-	const navigation = useNavigation();
-	const user = useSelector( state => state );
+    const navigation = useNavigation();
+    const dispatch = useDispatch();
+    const user = useSelector(state => state.user);
+    const [name, setName] = React.useState('');
 
-	const nick_name = "nick_studenta123";
+    const message = {
+        title: translate(Translations.DeleteConfirmTitle),
+        description: translate(Translations.DeleteConfirmDescription) + ' ' + translate(Translations.Account) + ' ?',
+    };
+
+    const updateUserData = () => {
+        API.user.update({nick_name: name}).then(() => {
+            dispatch(Actions.User.Update(name));
+            navigation.navigate(Routes.Profile);
+        })
+    }
+
+    const deleteAccount = () => Alert.alert(
+        message.title,
+        message.description,
+        [
+            {
+                text: "Cancel",
+                style: "cancel",
+            },
+            {
+                text: "OK",
+                onPress: () => {
+                },
+            },
+        ],
+    );
 
     return (
-		<MainWithNavigation>
-			
-			<TopBox style={{ alignItems: "center" }}>
-				<ProfileAvatar size={ 132 }/>
-				
-				<TouchableOpacity>
-					<Text style={[ GeneralStyles.text_regular, { color: ThemeStyles.blue_text, marginTop: 20 } ]}> 
-						{ translate( Translations.EditAvatar )}
-					</Text>
-				</TouchableOpacity>	
-			</TopBox> 
-
-			<ContainerWithScroll>
-				<Input
-					style={{marginBottom: 20}}
-					label={translate(Translations.ChangeYouNick)}
-					defaultValue={nick_name}
-				/>
-
-				<View style={{ marginTop: 50 }}>
-					<Button onPress={ () => navigation.navigate( Routes.ProfileEvents )} transparent_bg={ true }>
-						{ translate( Translations.YourEvents )}
-					</Button>
-
-					<Button>
-						{ translate( Translations.SaveChanges )}
-					</Button>
-
-					<TouchableOpacity>
-						<Text style={[ GeneralStyles.text_regular, { color: ThemeStyles.blue_text } ]}>
-							{ translate( Translations.DeleteAccount )}
-						</Text>
-					</TouchableOpacity>
-				</View>
-			</ContainerWithScroll>
-		</MainWithNavigation>
+        <MainWithNavigation>
+            <TopBox style={{alignItems: "center"}}>
+                <ProfileAvatar size={132}/>
+                <TouchableOpacity>
+                    <Text style={[GeneralStyles.text_regular, {color: ThemeStyles.blue_text, marginTop: 20}]}>
+                        {translate(Translations.EditAvatar)}
+                    </Text>
+                </TouchableOpacity>
+            </TopBox>
+            <ContainerWithScroll>
+                <Input
+                    style={{marginBottom: 20}}
+                    label={translate(Translations.ChangeYouNick)}
+                    defaultValue={user?.user_name}
+                    onChangeText={name => setName(name)}
+                />
+                <View style={{marginTop: 50}}>
+                    <Button onPress={() => navigation.navigate(Routes.ProfileEvents)} transparent_bg={true}>
+                        {translate(Translations.YourEvents)}
+                    </Button>
+                    <Button onPress={() => updateUserData()}>
+                        {translate(Translations.SaveChanges)}
+                    </Button>
+                    <TouchableOpacity onPress={() => deleteAccount()}>
+                        <Text style={[GeneralStyles.text_regular, {color: ThemeStyles.blue_text}]}>
+                            {translate(Translations.DeleteAccount)}
+                        </Text>
+                    </TouchableOpacity>
+                </View>
+            </ContainerWithScroll>
+        </MainWithNavigation>
     );
 };
