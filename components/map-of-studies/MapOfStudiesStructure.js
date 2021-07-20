936d3f5acece8a4c4ee63e3f35e35b96951c7a04
jarroot <jarroot1@gmail.com>
2021-07-20 12:00:37 +0300

    detecting map of study type

--- components/map-of-studies/MapOfStudiesStructure.js
+++ components/map-of-studies/MapOfStudiesStructure.js
@@ -1,10 +1,29 @@
-import React from 'react';
-import {View} from 'react-native';
+import React, { useMemo } from 'react';
+import { View } from 'react-native';
+import { useSelector } from 'react-redux';
+
+import bachelor_structure from "./structures/bachelor";
+import master_structure from "./structures/master";
 import mish_structure from "./structures/mish";
 
-const MapOfStudiesStructure = props => {
+const STRUCTURES = {
+	"(s1)": bachelor_structure,
+	"(s2)": master_structure,
+	"(sj)": mish_structure
+}
 
-	const structure = mish_structure;
+
+const MapOfStudiesStructure = () => {
+
+    const user_studies = useSelector( state => state.user?.studies );
+
+	const structure_type = useMemo(() => (
+		user_studies && !!user_studies.length
+			? user_studies.filter( item => item.year_of_study !== "absolwent" )?.[0]?.study?.level_of_study_short
+			: null
+	), []); 
+
+	const structure = STRUCTURES[ structure_type ];
 
 	return (
 		<View style={{ flex: 1, paddingBottom: 30 }}>
