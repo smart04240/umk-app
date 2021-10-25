f6e4bff76d194d47ae802eb6133a293141dd492d
georgemac <georgehopekek@gmail.com>
2021-10-25 12:38:16 +0200

    FIX ios float button issue

--- constants/Layout.js
+++ constants/Layout.js
@@ -1,7 +1,7 @@
 import {Dimensions, Platform} from "react-native";
 import * as Device from "expo-device";
 
-export const unibrowIOSDevices = (Platform.OS === 'ios' && Device.modelName?.match(/\d/g).join("") >= 10)
+export const unibrowIOSDevices = (Platform.OS === 'ios' && Device?.modelId?.match(/\d/g).join("") >= 10);
 const window = Dimensions.get('window');
 const paddingHorizontal = window.width < 375 ? 15 : 23;
 const calendarPickerPadding = window.width < 375 ? 6 : 23;
