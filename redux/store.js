426c6ed8377b6e12e9bfc3f3f2f0aa60fc072148
Dmytro Kardava <dima.kardava@gmail.com>
2021-11-16 16:18:22 +0100

    remove default middlewares

--- redux/store.js
+++ redux/store.js
@@ -1,6 +1,6 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import {combineReducers, configureStore, getDefaultMiddleware} from "@reduxjs/toolkit";
-import {FLUSH, PAUSE, PERSIST, persistReducer, persistStore, PURGE, REGISTER, REHYDRATE} from "redux-persist";
+import {combineReducers, configureStore} from "@reduxjs/toolkit";
+import {persistReducer, persistStore} from "redux-persist";
 import appReducer from "./reducers/appReducer";
 import userReducer from "./reducers/userReducer";
 import mapReducer from "./reducers/mapReducer";
@@ -8,7 +8,6 @@ import todosReducer from "./reducers/todosReducer";
 import notificationsReducer from "./reducers/notificationsReducer";
 import toastsReducer from "./reducers/toastsReducer";
 import eventCategoriesReducer from "./reducers/eventCategoriesReducer";
-import thunk from "redux-thunk";
 import calendarReducer from "./reducers/calendarReducer";
 import zdarzeniaReducer from "./reducers/zdarzeniaReducer";
 import badgesReducer from "./reducers/badgesReducer";
@@ -38,13 +37,10 @@ const persistedReducer = persistReducer(persistConfig, reducer);
 
 export const store = configureStore({
     reducer: persistedReducer,
-    middleware: getDefaultMiddleware({
-        serializableCheck: {
-            warnAfter: 55, // increase serializable check threshold
-            // must blacklist react-persist actions in react-toolkit
-            ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-        },
-    }).concat(thunk),
+    middleware: getDefaultMiddleware => getDefaultMiddleware({
+        serializableCheck: false,
+        immutableCheck: false,
+    }),
 });
 
 export const persistor = persistStore(store);
